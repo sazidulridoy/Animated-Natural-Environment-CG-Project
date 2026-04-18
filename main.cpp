e12c70017b474fdfc07be19460af00cada13e24f
sazidulridoy <ridoy2305101148@diu.edu.bd>
2026-04-18 14:31:33 +0600

    drawing sun

--- main.cpp
+++ main.cpp
@@ -2,11 +2,33 @@
 #include <math.h>
 #include <stdlib.h>
 
+float sunPos = 0.0f;
+
+void drawCircle(float cx, float cy, float r)
+{
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2f(cx, cy);
+    for (int i = 0; i <= 100; i++)
+    {
+        float angle = 2 * 3.1416 * i / 100;
+        glVertex2f(cx + cos(angle)*r, cy + sin(angle)*r);
+    }
+    glEnd();
+}
+
+void drawSun()
+{
+
+        glColor3f(1,0.9,0);
+        drawCircle(70 + sunPos, 80, 8);
+
+}
 
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glPushMatrix();
+    drawSun();
 
     glPopMatrix();
     glutSwapBuffers();
