ddd19a9392fd8d2a28e7cc324d08da52b1f37163
sazidulridoy <ridoy2305101148@diu.edu.bd>
2026-04-18 14:04:46 +0600

    basic changes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,35 @@
 #include <GL/glut.h>
 #include <math.h>
 #include <stdlib.h>
+
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPushMatrix();
+
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+void init()
+{
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-100,100,-100,100);
+}
+
+int main(int argc,char** argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(900,600);
+    glutCreateWindow("Animated Natural Environment");
+
+    init();
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+    return 0;
+}
